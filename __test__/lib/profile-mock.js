4dc4128ce998ab0283340016ab8a043712857655
Ron Barrantes <ronbarrantes@gmail.com>
2018-02-10 22:16:33 -0800

    test sample commit

---
+++ __test__/lib/profile-mock.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import faker from 'faker'
+import accountMock from './account-mock.js'
+//import Account from '../../src/model/account.js'
+
+
+export const create = () => { }
+export const createMany = (num) => {
+  return Promise.all(new Array(num).fill(0).map(() => create()))
+}
+export const remove = () => { }
