8e1d34e0244a1b13a209a4034d075ba7d3c86bf9
Ron Barrantes <ronbarrantes@gmail.com>
2018-02-11 17:13:11 -0800

    TEST COMMIT somethting

--- __test__/lib/profile-mock.js
+++ __test__/lib/profile-mock.js
@@ -4,9 +4,8 @@ import faker from 'faker'
 import accountMock from './account-mock.js'
 //import Account from '../../src/model/account.js'
 
-
 export const create = () => { }
 export const createMany = (num) => {
   return Promise.all(new Array(num).fill(0).map(() => create()))
 }
-export const remove = () => { }
+export const remove = () => { } 
