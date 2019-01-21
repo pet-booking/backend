663127e686ce8ff7d713c8126d645afaabea5fe6
ronbarrantes <ronbarrantes@gmail.com>
2019-01-20 21:05:11 -0800

    Add main and server

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
+'use strict'
+
 require('dotenv').config()
-const express = require('express')
-const app = express()
-
-const port = process.env.PORT || 1337
-
-app.get('/', (req, res)=> {
-  res.json({hello: "world"})
-})
-
-app.listen(port, () => console.log('SERVER UP @', port))
+require('@babel/register')
+require('./src/main.js')
---
+++ src/lib/server.js
@@ -0,0 +1,49 @@
+import cors from 'cors'
+import morgan from 'morgan'
+import express from 'express'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+
+const app = express()
+const jsonParser = bodyParser.json()
+const production = process.env.NODE_ENV === 'dev'
+let server = null
+console.log('__NODE_ENV__', process.env.NODE_ENV)
+
+// REGISTER MIDDLEWARE
+app.use(jsonParser)
+app.use(cors({ origin: process.env.CORS_ORIGIN }))
+app.use(morgan(production ? 'combined' : 'dev'))
+
+// 
+app.get('/', (req, res)=> {
+  res.json({ TLCSitters: "For U" })
+})
+
+//final 404
+app.all('*', (req, res) => res.sendStatus(404))
+
+export const start = () => {
+  return new Promise((resolve, reject) => {
+    if (server)
+      return reject(new Error('__SERVER_ERROR__ server already on'))
+    server = app.listen(process.env.PORT, () => {
+      console.log('__SERVER_ON__', process.env.PORT)
+      return resolve()
+    })
+  })
+    // .then(() => mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }))
+}
+export const stop = () => {
+  return new Promise((resolve, reject) => {
+    if (!server)
+      return reject(new Error('__SERVER_ERROR__ server already off'))
+    server.close(() => {
+      server = null
+      console.log('__SERVER_OFF__')
+      return resolve()
+    })
+  })
+    // .then(() => mongoose.disconnect())
+}
+
