60655fe211dd63f209c4e9063397ff8df92fd71f
ronbarrantes <ronbarrantes@gmail.com>
2019-07-20 22:36:12 -0700

    Change the linter

--- src/models/account.js
+++ src/models/account.js
@@ -1,11 +1,19 @@
 import { Schema } from 'mongoose'
+import bcrypt from 'bcrypt'
+import crypto from 'crypto'
+import jwt from 'jsonwebtoken'
+import httpErrors from 'http-errors'
 
 const accountSchema = new Schema({
-  username:{type: String, required: true, unnique: true},
-  email:{type: String, required: true, unnique: true},
-  passwordHash:{type: String, required: true, unnique: true},
-  tokenSeed:{type: String, required: true, unnique: true},
-  created:{type: Date, default: () => new Date()},
+  username:{ type: String, required: true, unique: true },
+  email:{ type: String, required: true, unique: true },
+  passwordHash:{ type: String, required: true, unique: true },
+  tokenSeed:{ type: String, required: true, unique: true },
+  created:{ type: Date, default: () => new Date() },
 })
+
+// instance methods
+
+
 
 export default accountSchema
