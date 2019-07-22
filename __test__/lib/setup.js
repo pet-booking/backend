637a4ee1382d23325da90a0857d8110de7f3ebf8
ronbarrantes <ronbarrantes@gmail.com>
2019-07-21 22:23:30 -0700

    Begin test

---
+++ __test__/lib/setup.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// Mock the environment
+process.env.PORT = 7000
+process.env.MONGODB_URI = 'mongodb://localhost/testing'                                          
+process.env.CORS_ORIGIN = 'http://localhost:8080'
+process.env.CLOUD_SECRET = 'awlfkawlkfmaglksemglskmgslkefmslekgmsgdlkwelkwn'
