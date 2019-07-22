5e6dc213ed777590967f4c327e68b535a99f0468
ronbarrantes <ronbarrantes@gmail.com>
2019-07-21 22:02:01 -0700

    Finish the auth route for now

--- src/routes/auth-router.js
+++ src/routes/auth-router.js
@@ -6,28 +6,36 @@ import basicAuth from '../lib/basic-auth-middleware'
 const authRouter =  new Router()
 
 authRouter
-  .post('/auth', basicAuth, (req, res, next) => {
+  .post('/auth', (req, res, next) => {
     Account.create(req.body)
       .then(account => {
-        console.log({ account })
         return account.tokenCreate()
       })
       .then(token => {
         res.cookie('X-TLC-Token', token, { maxAge: 604800000 })
-        console.log({ token })
         return res.json({ token })
       })
       .catch(next)
   })
 
-  .get('/auth', (req, res, next) => {
-    console.log('GETTING ROUTE AUTH ROUTE')
-    res.json({ auth: true })
+  .get('/auth', basicAuth, (req, res, next) => {
+    req.account.tokenCreate()
+      .then(token => {
+        res.cookie('X-TLC-Token', token, { maxAge: 604800000 })
+        return res.json({ token })
+      })
+      .catch(next)
   })
   
-  .put('/auth', (req, res, next) => {
-    console.log('POSTING AUTH')
-    res.json(req.body)
+  .put('/auth', basicAuth, (req, res, next) => {
+    console.log('<<-- BODY -->>', req.body)
+    if(!req.body.username || !req.body.email || !req.body.password)
+      return next(httpErrors(400, 'REQUEST_ERROR: username, email, and password required'))
+    req.account.update(req.body)
+      .then(() => {
+        res.sendStatus(200)
+      })
+      .catch(next)
   })
 
 export default authRouter
