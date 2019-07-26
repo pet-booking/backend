d254f2c8ae68fe496b31d208cc601df0c4831056
ronbarrantes <ronbarrantes@gmail.com>
2019-07-26 00:03:53 -0700

    PASSING: tesing create account

--- test/auth-router.test.js
+++ test/auth-router.test.js
@@ -3,35 +3,39 @@ require('@babel/register')
 const assert = require('assert')
 const superagent = require('superagent')
 const accountMock = require('./lib/accountMock')
+const server = require('../src/lib/server')
 
 const apiURL = `http://localhost:${process.env.PORT}`
 
 describe('Auth Route', ()=> {
-  
-  describe('fakey tests', ()=>{
-    it(`it will test if it's working`, ()=> {
-      assert.equal(1+2,3)
-    })
-    it(`it will check ${apiURL.split('http://')[1]}`, ()=> {
-      return superagent.get(apiURL)
-        .then(res => {
-          console.log(res.body)
-          assert.equal(res.status, 200)
-          assert.ok(res.body)
-        })
-    })
-  })
+  before(server.start)
+  afterEach(accountMock.remove)
+  after(server.stop)
 
   describe('/auth', () => {
-    it('should create a random user', ()=> {
+
+    it('should create a specific user', () => {
       return superagent.post(`${apiURL}/api/auth`)
-        .send(accountMock.fakeUser())
+        .send({
+          username: 'shark',
+          email: 'shark@inthedark.com',
+          password: 'sharkies',
+        })
         .then(res => {
-          console.log('RESPONSE', res.body)
           assert.equal(res.status, 200)
           assert.ok(res.body.token)
         })
     })
+
+    it('should create a random user', ()=> {
+      return superagent.post(`${apiURL}/api/auth`)
+        .send(accountMock.fakeUser())
+        .then(res => {
+          assert.equal(res.status, 200)
+          assert.ok(res.body.token)
+        })
+    })
+
   })
 })
 
