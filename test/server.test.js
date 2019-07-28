fd56a8f6dba75ce13b048742dfbbed6ff7b0149d
ronbarrantes <ronbarrantes@gmail.com>
2019-07-28 11:57:31 -0700

    Change from assert to chai

---
+++ test/server.test.js
@@ -0,0 +1,32 @@
+require('./lib/setup')
+require('@babel/register')
+// const assert = require('assert')
+const expect = require('chai').expect
+const superagent = require('superagent')
+const server = require('../src/lib/server')
+
+const apiURL = `http://localhost:${process.env.PORT}`
+
+
+describe('Server file', ()=> {
+  before(server.start)
+  after(server.stop)
+
+  describe('root route - 200', ()=>{
+    it('expect root message', ()=>{
+      return superagent.get(apiURL)
+        .then(res => {
+          expect(res.status).to.equal(200)
+          expect(res.body.message).to.exist
+        })
+    })
+
+    it('expect route not found - 404', ()=>{
+      return superagent.get(`${apiURL}/not_real`)
+        .then(Promise.reject)
+        .catch(res => {
+          console.log('STATUS -->', res.status)
+        })
+    })
+  })
+})
