9ba5ad24ff8cdb688bb414aed29aa62bddd85fe3
ronbarrantes <ronbarrantes@gmail.com>
2019-07-28 11:55:14 -0700

    Clean up code

--- src/middleware/four-oh-four.js
+++ src/middleware/four-oh-four.js
@@ -3,6 +3,5 @@ import createError from 'http-errors'
 
 // INTERFACE
 export default (req, res, next) => {
-  res.send('404 Not Found :(')
   return next(createError(404, `USER ERROR: ${req.url} not a route`))
 }
